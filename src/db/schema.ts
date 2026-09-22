d749095d4a9fd5271d8c807c19b6c3e5f8851d5f
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2026-09-21 20:01:43 -0500

    schema updates

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,59 @@
 import {
 	foreignKey,
 	integer,
+	primaryKey,
 	sqliteTable,
 	text,
 } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users_table", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	name: text("name"),
+	createdAt: integer("created_at", { mode: "timestamp_ms" })
+		.notNull()
+		.$defaultFn(() => new Date()),
+	updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+		.notNull()
+		.$defaultFn(() => new Date())
+		.$onUpdateFn(() => new Date()),
+});
+
+export const teams = sqliteTable("teams_table", {
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	name: text("name"),
+	createdAt: integer("created_at", { mode: "timestamp_ms" })
+		.notNull()
+		.$defaultFn(() => new Date()),
+	updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+		.notNull()
+		.$defaultFn(() => new Date())
+		.$onUpdateFn(() => new Date()),
+});
+
+export const teamMembers = sqliteTable(
+	"team_members_table",
+	{
+		teamId: text("team_id").references(() => teams.id),
+		userId: text("user_id").references(() => users.id),
+		role: text("role", { enum: ["admin", "member"] }),
+		createdAt: integer("created_at", { mode: "timestamp_ms" })
+			.notNull()
+			.$defaultFn(() => new Date()),
+		updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+			.notNull()
+			.$defaultFn(() => new Date())
+			.$onUpdateFn(() => new Date()),
+	},
+	(table) => [
+		primaryKey({
+			columns: [table.teamId, table.userId],
+		}),
+	],
+);
 
 export const tasksTable = sqliteTable(
 	"tasks_table",
@@ -11,10 +61,15 @@ export const tasksTable = sqliteTable(
 		id: text("id")
 			.primaryKey()
 			.$defaultFn(() => crypto.randomUUID()),
-		status: text("status").notNull().default("not_started"),
+		status: text("status", {
+			enum: ["not_started", "in_progress", "completed"],
+		})
+			.notNull()
+			.default("not_started"),
 		name: text("name").notNull(),
 		description: text("description"),
 		parentId: text("parent_id"),
+		assignedTo: text("assigned_to").references(() => users.id),
 		dueAt: integer("due_at", { mode: "timestamp_ms" }),
 		createdAt: integer("created_at", { mode: "timestamp_ms" })
 			.notNull()
